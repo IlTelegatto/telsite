0ca50c2b17b6ff4c9d356100379734d7e8378e6b
IlTelegatto <69989584+IlTelegatto@users.noreply.github.com>
2022-01-26 21:58:02 +0100

    Update telaibreak2.js

--- JsTelsite/telaibreak2.js
+++ JsTelsite/telaibreak2.js
@@ -1,75 +1,69 @@
 const prompts = [
-  ["hi", "hey", "hello", "good morning", "good afternoon"],
-  ["how are you", "how is life", "how are things"],
-  ["what are you doing", "what is going on", "what is up"],
-  ["how old are you"],
-  ["who are you", "are you human", "are you bot", "are you human or bot"],
-  ["who created you", "who made you"],
-  [
-    "your name please",
-    "your name",
-    "may i know your name",
-    "what is your name",
-    "what call yourself"
-  ],
-  ["i love you"],
-  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
-  ["bad", "bored", "tired"],
-  ["help me", "tell me story", "tell me joke"],
-  ["ah", "yes", "ok", "okay", "nice"],
-  ["bye", "good bye", "goodbye", "see you later"],
-  ["what should i eat today"],
-  ["bro"],
-  ["what", "why", "how", "where", "when"],
-  ["no","not sure","maybe","no thanks"],
-  [""],
-  ["haha","ha","lol","hehe","funny","joke"]
+["ciao", "ehi", "ciao", "buongiorno", "buon pomeriggio"],
+   ["come stai", "com'è la vita", "come stanno le cose"],
+   ["cosa stai facendo", "cosa sta succedendo", "cosa succede"],
+   ["quanti anni hai"],
+   ["chi sei", "sei umano", "sei un bot", "sei umano o un bot"],
+   ["chi ti ha creato", "chi ti ha creato"],
+   [
+     "il tuo nome, per favore",
+     "il tuo nome",
+     "potrei sapere il tuo nome",
+     "come ti chiami",
+     "come ti chiami"
+   ],
+   ["Ti voglio bene"],
+   ["felice", "buono", "divertente", "meraviglioso", "fantastico", "figo"],
+   ["cattivo", "annoiato", "stanco"],
+   ["aiutami", "raccontami una storia", "raccontami una barzelletta"],
+   ["ah", "sì", "ok", "okay", "bello"],
+   ["arrivederci", "arrivederci", "a dopo"],
+   ["cosa dovrei mangiare oggi"],
+   ["Fra"],
+   ["cosa", "perché", "come", "dove", "quando"],
+   ["no","non sono sicuro","forse","no grazie"],
+   [""],
+   ["haha","ha","lol","hehe","divertente","scherzo"]
 ]
-
-// Possible responses, in corresponding order
 
 const replies = [
-  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
-  [
-    "Fine... how are you?",
-    "Pretty well, how are you?",
-    "Fantastic, how are you?"
-  ],
-  [
-    "Nothing much",
-    "About to go to sleep",
-    "Can you guess?",
-    "I don't know actually"
-  ],
-  ["I am infinite"],
-  ["I am just a bot", "I am a bot. What are you?"],
-  ["Il Telegatto"],
-  ["Telai"],
-  ["I love you too", "Me too"],
-  ["Have you ever felt bad?", "Glad to hear it"],
-  ["Why?", "Why? You shouldn't!", "Try watching TV"],
-  ["What about?", "Once upon a time..."],
-  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
-  ["Bye", "Goodbye", "See you later"],
-  ["Sushi", "Pizza"],
-  ["Bro!"],
-  ["Great question"],
-  ["That's ok","I understand","What do you want to talk about?"],
-  ["Please say something :("],
-  ["Haha!","Good one!"]
+["Ciao!", "Ciao!", "Ehi!", "Ciao!", "Buongiorno"],
+   [
+     "Bene, come stai?",
+     "Abbastanza bene, come stai?",
+     "Fantastico, come stai?"
+   ],
+   [
+     "Non molto",
+     "Sto per andare a dormire",
+     "Puoi indovinare?",
+     "Non lo so in realtà"
+   ],
+   ["Io sono infinito"],
+   ["Sono solo un bot", "Sono un bot. Cosa sei?"],
+   ["Il Telegatto"],
+   ["Telai"],
+   ["Ti amo anch'io", "Anche io"],
+   ["Ti sei mai sentito male?", "Felice di sentirlo"],
+   ["Perché?", "Perché? Non dovresti!", "Prova a guardare la TV"],
+   ["Che ne dici?", "C'era una volta..."],
+   ["Raccontami una storia", "Raccontami una barzelletta", "Raccontami di te"],
+   ["Ciao", "Arrivederci", "Ci vediamo dopo"],
+   ["Sushi", "Pizza"],
+   ["Fra!"],
+   ["Ottima domanda"],
+   ["Va bene","Capisco","Di cosa vuoi parlare?"],
+   ["Per favore dì qualcosa :("],
+   ["Haha!", "Bravo!"]
 ]
-
-// Random for any other user input
 
 const alternative = [
-  "Same",
-  "Go on...",
-  "Bro...",
-  "Try again",
-  "I'm listening...",
-  "I don't understand :/"
+"hmm chiedimi qualcosa",
+   "Vai avanti...",
+   "Fra...",
+   "Riprova",
+   "capisco tutto...",
+   "Non capisco :/"
 ]
 
-// Whatever else you want :)
-
-const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
+const coronavirus = ["Per favore resta a casa", "Indossa una mascherina", "Per fortuna non ho il COVID", "Questi sono tempi incerti"]
